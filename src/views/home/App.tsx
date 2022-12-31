c4c9e087f3cbc92ff351daf4941a1437d1f15868
loczippo <kimloc1000@gmail.com>
2022-12-31 08:11:37 -0800

    add taiwindcss

---
+++ src/views/home/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function WebApplication() {
+  return (
+    <div className="App">
+      <h1 className="flex">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default WebApplication;
