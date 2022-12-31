9787c2f1bd737eb86a18209cfebaa69ae6cb5c9a
loczippo <kimloc1000@gmail.com>
2022-12-31 08:08:18 -0800

    init base source

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './assets/css/tailwind.css';
+import WebApplication from './views/home/App';
+// import reportWebVitals from '../reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <WebApplication />
   </React.StrictMode>
 );
 
-reportWebVitals();
+// reportWebVitals();
