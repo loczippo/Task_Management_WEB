c6be3549e1e90d2f99ad90cfd4fbf7bf76a8da80
loczippo <kimloc1000@gmail.com>
2023-01-02 05:27:48 -0800

    update base navbar

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,1 +1,12 @@
-export {}
+
+import React from 'react';
+
+function Home() {
+  return (
+    <div className=''>
+        <h1>Home component</h1>
+    </div>
+  );
+}
+
+export default Home;
--- src/views/home/App.tsx
+++ src/views/home/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Sliderbar, Home } from '../../components/'
 
 function WebApplication() {
   return (
-    <div className="App">
-      <h1 className="flex">
-      Hello world!
-    </h1>
-    </div>
+    <>
+      <div className='flex'>
+        <Sliderbar/>
+        <div className='group/main ml-16'>
+        <button className='group-hover/main:left-11 bg-red-500'>Toggle</button>
+          <section className="border bg-red-200 absolute transition-all duration-500 -left-60 group-hover/main:left-11">
+                <p className="h-screen grid place-items-center w-60">sidebar</p>
+          </section>
+        </div>
+        <div className='ml-20'><Home/></div>
+      </div>
+      
+    </>
   );
 }
 
