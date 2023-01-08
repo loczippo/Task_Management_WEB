a2f71d76f8a365920825405558072aae978ddd60
loczippo <kimloc1000@gmail.com>
2023-01-08 08:39:45 -0800

    declare routes view

---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import React from 'react';
+import { HomeView, NewsView, ErrorView } from '../views';
+import { ReactJSXElement } from '@emotion/react/types/jsx-namespace';
+
+function AppRouter(): ReactJSXElement {
+    return (
+        <Routes>
+            <Route path='/news' element={<NewsView/>} />
+            <Route path='/' element={<HomeView/>} />
+            <Route path='*' element={<ErrorView/>} />
+        </Routes>
+    )
+}
+
+export default AppRouter;
