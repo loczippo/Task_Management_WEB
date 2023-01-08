c990539aa0a828e1af1b1f5e5e9a0aed3df1ae15
loczippo <kimloc1000@gmail.com>
2023-01-08 08:47:52 -0800

    create sliderbar enum

---
+++ src/enums/sliderbar.enum.ts
@@ -0,0 +1,16 @@
+enum PrimarySliderBarEnum {
+    DEFAULT = 'default',
+    SETTINGS = 'setings',
+    USERS = 'users',
+}
+
+enum ToggleSliderBarEnum {
+    KEY = 'toggle',
+    VALUE = 'true',
+}
+
+enum LabelSliderBarEnum {
+    KEY = 'sliderbar_menu',
+}
+
+export { PrimarySliderBarEnum, ToggleSliderBarEnum, LabelSliderBarEnum }
