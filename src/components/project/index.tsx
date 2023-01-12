37f88d9241b50192a6e9d2148bc35b06c39354ee
loczippo <kimloc1000@gmail.com>
2023-01-12 08:14:20 -0800

    navlink to url with props val

--- src/components/project/index.tsx
+++ src/components/project/index.tsx
@@ -9,7 +9,7 @@ import { useState } from "react";
 
 function Project(props: any): ReactJSXElement {
 
-    const {projectName, projectId} = props;
+    const { projectName, projectId, timeLine, board } = props;
 
     const [toggleProject, setToggleProject] = useState(false);
 
@@ -17,47 +17,47 @@ function Project(props: any): ReactJSXElement {
         setToggleProject(!toggleProject);
     }
 
-    
+
     return (
         <div className="sliderbar_header flex flex-col w-full pb-1">
             {
-            !(toggleProject)
-            ?
-             <Link onClick={hanldeToggleProject} to={`/timeline/${projectId}`} className='relative group/item group/icon flex justify-start pb-[2px] pl-3 pr-2 items-center align-middle text-[12.5px] font-medium leading-6 cursor-pointer hover:bg-[var(--hover-color)]'>
-                <div className={`duration-300 transition-all ${toggleProject ? 'rotate-90': ''}`}>
-                    <ArrowRightIcon className='mediumIcon' />
-                </div>
-                <Brightness1Icon className={`text-red-500 miniIcon pl-1 pr-1`} />
-                <h1 className='pl-1'>{projectName}</h1>
-                <div className='absolute right-[9px] invisible duration-100 ease-in-out transform-all group-hover/item:visible'>
-                    <MoreVertIcon className='primaryIcon text-[var(--blur-text)]' />
-                </div>
-            </Link>
-            :
-            <div onClick={hanldeToggleProject} className='relative group/item group/icon flex justify-start pb-[2px] pl-3 pr-2 items-center align-middle text-[12.5px] font-medium leading-6 cursor-pointer hover:bg-[var(--hover-color)]'>
-                <div className={`duration-300 transition-all ${toggleProject ? 'rotate-90': ''}`}>
-                    <ArrowRightIcon className='mediumIcon' />
-                </div>
-                <Brightness1Icon className={`text-red-500 miniIcon pl-1 pr-1`} />
-                <h1 className='pl-1'>{projectName}</h1>
-                <div className='absolute right-[9px] invisible duration-100 ease-in-out transform-all group-hover/item:visible'>
-                    <MoreVertIcon className='primaryIcon text-[var(--blur-text)]' />
-                </div>
-            </div>
+                !(toggleProject)
+                    ?
+                    <Link onClick={hanldeToggleProject} to={`/${timeLine ? "timeline" : "board"}/${projectId}`} className='relative group/item group/icon flex justify-start pb-[2px] pl-3 pr-2 items-center align-middle text-[12.5px] font-medium leading-6 cursor-pointer hover:bg-[var(--hover-color)]'>
+                        <div className={`duration-300 transition-all ${toggleProject ? 'rotate-90' : ''}`}>
+                            <ArrowRightIcon className='mediumIcon' />
+                        </div>
+                        <Brightness1Icon className={`text-red-500 miniIcon pl-1 pr-1`} />
+                        <h1 className='pl-1'>{projectName}</h1>
+                        <div className='absolute right-[9px] invisible duration-100 ease-in-out transform-all group-hover/item:visible'>
+                            <MoreVertIcon className='primaryIcon text-[var(--blur-text)]' />
+                        </div>
+                    </Link>
+                    :
+                    <div onClick={hanldeToggleProject} className='relative group/item group/icon flex justify-start pb-[2px] pl-3 pr-2 items-center align-middle text-[12.5px] font-medium leading-6 cursor-pointer hover:bg-[var(--hover-color)]'>
+                        <div className={`duration-300 transition-all ${toggleProject ? 'rotate-90' : ''}`}>
+                            <ArrowRightIcon className='mediumIcon' />
+                        </div>
+                        <Brightness1Icon className={`text-red-500 miniIcon pl-1 pr-1`} />
+                        <h1 className='pl-1'>{projectName}</h1>
+                        <div className='absolute right-[9px] invisible duration-100 ease-in-out transform-all group-hover/item:visible'>
+                            <MoreVertIcon className='primaryIcon text-[var(--blur-text)]' />
+                        </div>
+                    </div>
             }
             <div className={!toggleProject ? 'hidden' : ''}>
-                <div className='flex flex-col w-full'>
+                {(timeLine) ? <div className='flex flex-col w-full'>
                     <NavLink to={`/timeline/${projectId}`} className='flex justify-start pt-[2px] pb-[2px] pl-10 pr-2 items-center align-middle text-[12.5px] font-medium leading-6 cursor-pointer hover:bg-[var(--hover-color)]'>
                         <VerticalSplitIcon className='bodyIcon' />
                         <h1 className='pl-3'>Timeline</h1>
                     </NavLink>
-                </div>
-                <div className='flex flex-col w-full'>
+                </div> : null}
+                {(board) ? <div className='flex flex-col w-full'>
                     <NavLink to={`/board/${projectId}`} className='flex justify-start pt-[2px] pb-[2px] pl-10 pr-2 items-center align-middle text-[12.5px] font-medium leading-6 cursor-pointer hover:bg-[var(--hover-color)]'>
                         <TableChartIcon className='bodyIcon' />
                         <h1 className='pl-3'>Board</h1>
                     </NavLink>
-                </div>
+                </div> : null}
             </div>
         </div>
     );
--- src/components/team/index.tsx
+++ src/components/team/index.tsx
@@ -9,7 +9,7 @@ function Team(props: any): ReactJSXElement {
     return (
 
         <div className="sliderbar_header flex flex-col w-full pb-1">
-            <NavLink to={`/${teamId}`} className='relative group flex justify-start pt-1 pb-1 pl-3 pr-2 items-center align-middle text-[12.5px] font-medium leading-6 cursor-pointer hover:bg-[var(--hover-color)]'>
+            <NavLink to={`/team/${teamId}`} className='relative group flex justify-start pt-1 pb-1 pl-3 pr-2 items-center align-middle text-[12.5px] font-medium leading-6 cursor-pointer hover:bg-[var(--hover-color)]'>
                 <PeopleIcon className='bodyIcon' />
                 <h1 className='pl-3'>{teamName}</h1>
                 <div className='absolute right-[9px] invisible duration-100 easeo-in-out transfrm-all group-hover:visible'>
