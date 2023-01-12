7297737437c84ea15f393e17d179bb1744c89b92
loczippo <kimloc1000@gmail.com>
2023-01-12 08:13:50 -0800

    update seting second sliderbar

--- src/components/sliderbars/seconds/settings.tsx
+++ src/components/sliderbars/seconds/settings.tsx
@@ -1,13 +1,17 @@
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import PeopleIcon from '@mui/icons-material/People';
-import PersonIcon from '@mui/icons-material/Person';
-import PaymentIcon from '@mui/icons-material/Payment';
+
 import { useAppSelector } from '../../../hooks'
 import { NavLink, Link } from 'react-router-dom';
 import { setSecondSliderBarMenu } from '../../../stores/sliderbar'
 import { useAppDispatch } from '../../../hooks'
 import { PrimarySliderBarEnum } from '../../../enums/'
 import { setSliderBarMenu } from '../../../utils';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import PeopleIcon from '@mui/icons-material/People';
+import PersonIcon from '@mui/icons-material/Person';
+import PaymentIcon from '@mui/icons-material/Payment';
+import HelpIcon from '@mui/icons-material/Help';
+import LockIcon from '@mui/icons-material/Lock';
+import IntegrationInstructionsIcon from '@mui/icons-material/IntegrationInstructions';
 
 function SliderBarSecondSettings() {
     const isToggle = useAppSelector(state => state.sliderbar.value)
@@ -24,58 +28,58 @@ function SliderBarSecondSettings() {
 
 
 
-  return (
-    <div className='Sidebar_wrapper h-full overflow-hidden duration-500 ease-in-out transform'>
-    <section className={`overflow-hidden flex h-full flex-col items-center justify-between bg-[var(--second-sliderbar)] text-[var(--white-text)] transition-all ${(isToggle) ? "w-0" : "w-60"}`}>
-        <div className="sliderbar_header relative flex flex-col w-full">
-            <div className="h-14">
-                <Link to="" className='flex pl-4 pt-3 pr-2 h-12 items-center' onClick={() => SetSecondSliderBar(PrimarySliderBarEnum.DEFAULT)}>
-                    <ArrowBackIosIcon className='avgIcon' />
-                    <div  className='text-[var(--white-text)] text-[13px] pl-3 font-[450]'>Return to workspace</div>
-                </Link>
-            </div>
+    return (
+        <div className='Sidebar_wrapper h-full overflow-hidden duration-500 ease-in-out transform'>
+            <section className={`overflow-hidden flex h-full flex-col items-center justify-between bg-[var(--second-sliderbar)] text-[var(--white-text)] transition-all ${(isToggle) ? "w-0" : "w-60"}`}>
+                <div className="sliderbar_header relative flex flex-col w-full">
+                    <div className="h-14">
+                        <Link to="" className='flex pl-4 pt-3 pr-2 h-12 items-center' onClick={() => SetSecondSliderBar(PrimarySliderBarEnum.DEFAULT)}>
+                            <ArrowBackIosIcon className='avgIcon' />
+                            <div className='text-[var(--white-text)] text-[13px] pl-3 font-[450]'>Return to workspace</div>
+                        </Link>
+                    </div>
+                </div>
+                <div className='scrollbar flex flex-col w-full h-full pt-3 pb-1'>
+                    <div className="sliderbar_header">
+                        <NavLink to="/setting/me/profile" className='flex justify-start mb-6 pt-1 pb-1 pl-3 pr-2 items-center align-middle text-[13px] font-[450] leading-6 cursor-pointer hover:bg-[var(--hover-color)]'>
+                            <PersonIcon className='bodyIcon' />
+                            <h1 className='pl-3'>My profile</h1>
+                        </NavLink>
+                    </div>
+                    <div className="sliderbar_header">
+                        <NavLink to="/setting/users" className='flex justify-start mb-6 pt-1 pb-1 pl-3 pr-2 items-center align-middle text-[13px] font-[450] leading-6 cursor-pointer hover:bg-[var(--hover-color)]'>
+                            <PeopleIcon className='bodyIcon' />
+                            <h1 className='pl-3'>Users</h1>
+                        </NavLink>
+                    </div>
+                    <div className="sliderbar_header">
+                        <NavLink to="/setting/subcription" className='flex justify-start mb-6 pt-1 pb-1 pl-3 pr-2 items-center align-middle text-[13px] font-[450] leading-6 cursor-pointer hover:bg-[var(--hover-color)]'>
+                            <PaymentIcon className='bodyIcon' />
+                            <h1 className='pl-3'>Subcription</h1>
+                        </NavLink>
+                    </div>
+                    <div className="sliderbar_header">
+                        <NavLink to="/setting/app" className='flex justify-start mb-6 pt-1 pb-1 pl-3 pr-2 items-center align-middle text-[13px] font-[450] leading-6 cursor-pointer hover:bg-[var(--hover-color)]'>
+                            <IntegrationInstructionsIcon className='bodyIcon' />
+                            <h1 className='pl-3'>Apps & integrations</h1>
+                        </NavLink>
+                    </div>
+                    <div className="sliderbar_header">
+                        <NavLink to="/setting/2fa" className='flex justify-start mb-6 pt-1 pb-1 pl-3 pr-2 items-center align-middle text-[13px] font-[450] leading-6 cursor-pointer hover:bg-[var(--hover-color)]'>
+                            <LockIcon className='bodyIcon' />
+                            <h1 className='pl-3'>Two Factor Authentication</h1>
+                        </NavLink>
+                    </div>
+                    <div className="sliderbar_header">
+                        <NavLink to="/setting/shortcut" className='flex justify-start mb-6 pt-1 pb-1 pl-3 pr-2 items-center align-middle text-[13px] font-[450] leading-6 cursor-pointer hover:bg-[var(--hover-color)]'>
+                            <HelpIcon className='bodyIcon' />
+                            <h1 className='pl-3'>Help & shortcuts</h1>
+                        </NavLink>
+                    </div>
+                </div>
+            </section>
         </div>
-        <div className='scrollbar flex flex-col w-full h-full pt-3 pb-1'>
-            <div className="sliderbar_header">
-                <NavLink to="/setting/me/profile" className='flex justify-start mb-6 pt-1 pb-1 pl-3 pr-2 items-center align-middle text-[13px] font-[450] leading-6 cursor-pointer hover:bg-[var(--hover-color)]'>
-                    <PersonIcon className='bodyIcon' />
-                    <h1 className='pl-3'>My profile</h1>
-                </NavLink>
-            </div>
-            <div className="sliderbar_header">
-                <NavLink to="/setting/users" className='flex justify-start mb-6 pt-1 pb-1 pl-3 pr-2 items-center align-middle text-[13px] font-[450] leading-6 cursor-pointer hover:bg-[var(--hover-color)]'>
-                    <PeopleIcon className='bodyIcon' />
-                    <h1 className='pl-3'>Users</h1>
-                </NavLink>
-            </div>
-            <div className="sliderbar_header">
-                <NavLink to="/setting/subcription" className='flex justify-start mb-6 pt-1 pb-1 pl-3 pr-2 items-center align-middle text-[13px] font-[450] leading-6 cursor-pointer hover:bg-[var(--hover-color)]'>
-                    <PaymentIcon className='bodyIcon' />
-                    <h1 className='pl-3'>Subcription</h1>
-                </NavLink>
-            </div>
-            <div className="sliderbar_header">
-                <NavLink to="/setting/subcription" className='flex justify-start mb-6 pt-1 pb-1 pl-3 pr-2 items-center align-middle text-[13px] font-[450] leading-6 cursor-pointer hover:bg-[var(--hover-color)]'>
-                    <PaymentIcon className='bodyIcon' />
-                    <h1 className='pl-3'>Apps & integrations</h1>
-                </NavLink>
-            </div>
-            <div className="sliderbar_header">
-                <NavLink to="/setting/subcription" className='flex justify-start mb-6 pt-1 pb-1 pl-3 pr-2 items-center align-middle text-[13px] font-[450] leading-6 cursor-pointer hover:bg-[var(--hover-color)]'>
-                    <PaymentIcon className='bodyIcon' />
-                    <h1 className='pl-3'>Single Sign On</h1>
-                </NavLink>
-            </div>
-            <div className="sliderbar_header">
-                <NavLink to="/setting/subcription" className='flex justify-start mb-6 pt-1 pb-1 pl-3 pr-2 items-center align-middle text-[13px] font-[450] leading-6 cursor-pointer hover:bg-[var(--hover-color)]'>
-                    <PaymentIcon className='bodyIcon' />
-                    <h1 className='pl-3'>Help & shortcuts</h1>
-                </NavLink>
-            </div>
-        </div>
-    </section>
-</div>
-  );
+    );
 }
 
 export default SliderBarSecondSettings;
