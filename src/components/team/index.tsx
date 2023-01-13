a9965f3af2d71af096165233169a9937d4c209dc
loczippo <kimloc1000@gmail.com>
2023-01-13 06:43:44 -0800

    add skeleton loading

--- src/components/team/index.tsx
+++ src/components/team/index.tsx
@@ -2,6 +2,8 @@ import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
 import { NavLink } from "react-router-dom";
 import PeopleIcon from '@mui/icons-material/People';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 function Team(props: any): ReactJSXElement {
 
@@ -11,7 +13,7 @@ function Team(props: any): ReactJSXElement {
         <div className="sliderbar_header flex flex-col w-full pb-1">
             <NavLink to={`/team/${teamId}`} className='relative group flex justify-start pt-1 pb-1 pl-3 pr-2 items-center align-middle text-[12.5px] font-medium leading-6 cursor-pointer hover:bg-[var(--hover-color)]'>
                 <PeopleIcon className='bodyIcon' />
-                <h1 className='pl-3'>{teamName}</h1>
+                <h1 className='pl-3'>{teamName || <Skeleton />}</h1>
                 <div className='absolute right-[9px] invisible duration-100 easeo-in-out transfrm-all group-hover:visible'>
                     <MoreVertIcon className='primaryIcon text-[var(--blur-text)]' />
                 </div>
